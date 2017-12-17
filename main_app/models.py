dd47103da3f27f9ef0d8310a5aa1d89118f469ce
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-17 17:46:53 +0530

    add model for compute resource

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class compute_resource(models.Model):
+    name = models.CharField(max_length=10)
+    ip_address = models.CharField(max_length=15)
+    root_password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
