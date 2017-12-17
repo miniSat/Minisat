f57c990eb84a3ef61857d651e39e4bf5eb5565b6
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-17 17:50:11 +0530

    pass object for form in dictionary and create function post_data to commit value to database

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,20 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from .forms import compute_resource_form
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
 def compute_resource(request):
-    return render(request,'compute_resource.html',{'title_name':'Create New Compute Resource'})
+    form = compute_resource_form()
+    return render(request,'compute_resource.html',{'title_name':'Create New Compute Resource', 'form':form})
+
+def post_data(request):
+    form = compute_resource_form(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+
+    return HttpResponseRedirect('/')
