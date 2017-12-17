579bc4b57f9d59a50aa2ed542c056106b458a21d
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-17 17:50:53 +0530

    redirect post_data to post_data function

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('compute_resource', views.compute_resource, name='compute_resource')
+    path('compute_resource', views.compute_resource, name='compute_resource'),
+    path('post_data', views.post_data, name='post_data'),
 ]
