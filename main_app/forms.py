f414edd581ca66a88b52eb9b529699af8ac1c7d3
khomesh24 <khomeshthakre24@gmail.com>
2017-12-18 11:29:26 +0530

    Added profile_form
    Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import compute_resource
+from .models import compute_resource,profile
 
 class compute_resource_form(forms.ModelForm):
     class Meta:
         model = compute_resource
         fields = ['name', 'ip_address', 'root_password']
+
+
+class profile_form(forms.ModelForm):
+    class Meta:
+        model = profile
+        fields = ['profile_name','ram','cpus','disk_size']
