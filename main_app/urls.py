def4aa3b38bb2b1d4b796fe3656ec237e5b359e5
khomesh24 <khomeshthakre24@gmail.com>
2017-12-18 11:29:56 +0530

    Added URL entry for dispatcher
    Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('compute_resource', views.compute_resource, name='compute_resource'),
     path('post_data', views.post_data, name='post_data'),
+    path('profile',views.profile, name='profile'),
+    path('post_profile',views.post_profile,name="post_profile"),
+
 ]
