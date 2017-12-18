b426f49fdfe5faa0e6d1a22af0d478a44018cb54
khomesh24 <khomeshthakre24@gmail.com>
2017-12-18 11:31:36 +0530

    Register profile in admin panel
    Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import compute_resource
+from .models import compute_resource,profile
 # Register your models here.
 
 admin.site.register(compute_resource)
+admin.site.register(profile)
