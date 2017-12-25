68b73d7f52100d32a70613348350d5533df3bf8d
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-26 00:37:53 +0530

    add def for redirecting to create host and to create virt-install command
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from .forms import compute_resource_form,profile_form
-from django.http import HttpResponseRedirect
-from .models import compute_resource_model
+from .forms import compute_resource_form,profile_form,create_host_form
+from django.http import HttpResponseRedirect,HttpResponse
+from .models import compute_resource_model,profile_model
 
 
 # Create your views here.
@@ -30,3 +30,21 @@ def post_profile(request):
     if form.is_valid():
         form.save(commit=True)
     return HttpResponseRedirect('/')
+
+def create_host(request):
+    form = create_host_form()
+    return render(request,'create_host.html', {'title_name':'Create A New Host','form':form})
+
+def post_create_host(request):
+    form = create_host_form(request.POST)
+    if form.is_valid():
+        form_vm = form.cleaned_data['vm_name']
+        form_os = form.cleaned_data['vm_os']
+        form_profile = form.cleaned_data['select_vm_profile']
+        profile_details = list(profile_model.objects.filter(profile_name=form_profile).values_list()[0])
+        *not_imp1,ram,cpus,disk_size,compute_profile=profile_details
+        compute_details = list(compute_resource_model.objects.filter(name=compute_profile).values_list()[0])
+        *not_imp2,compute_ip,compute_passwd=compute_details
+        final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+form_vm+' --ram '+str(ram)+' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+form_vm+'.qcow2,bus=virtio,size='+str(disk_size)+' --network bridge:virbr0'
+    return render(request,'create_host.html',{'final_cmd':final_cmd})
+
