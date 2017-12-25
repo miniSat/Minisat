a843f2d99594aabf91586601d278e938c185bd96
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-26 00:36:54 +0530

    add path for create host
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post_data', views.post_data, name='post_data'),
     path('profile',views.profile, name='profile'),
     path('post_profile',views.post_profile,name="post_profile"),
+    path('create_host', views.create_host, name="create_host"),
+    path('post_create_host',views.post_create_host,name="post_create_host")
 ]
