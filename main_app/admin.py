205651229c50bd95d475aa5a73e3228cf4f7b143
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-12-26 00:34:47 +0530

    register model create host
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import compute_resource_model,profile_model
+from .models import compute_resource_model,profile_model,create_host_model
 # Register your models here.
 
 admin.site.register(compute_resource_model)
 admin.site.register(profile_model)
+admin.site.register(create_host_model)
