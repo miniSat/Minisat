78aed8cc1b3f772623743bff71fbf69903e05177
khomesh24 <khomeshthakre24@gmail.com>
2017-12-26 10:10:55 +0530

    register operating model to admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import compute_resource_model,profile_model,create_host_model
+from .models import compute_resource_model,profile_model,create_host_model,operating_system_model
 # Register your models here.
 
 admin.site.register(compute_resource_model)
 admin.site.register(profile_model)
 admin.site.register(create_host_model)
+admin.site.register(operating_system_model)
