d83dadbf5137f834290591c7fc9d93081b0dcba8
khomesh24 <khomeshthakre24@gmail.com>
2017-12-26 11:41:52 +0530

    implemented PEP-8 standard
    Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,20 +2,20 @@ from django import forms
 from .models import *
 
 
-class compute_resource_form(forms.ModelForm):
+class Compute_resource_form(forms.ModelForm):
     class Meta:
         model = Compute_resource_model
         fields = '__all__'
 
 
 
-class profile_form(forms.ModelForm):
+class Profile_form(forms.ModelForm):
     class Meta:
         model = Profile_model
         fields = '__all__'
 
 
-class create_host_form(forms.ModelForm):
+class Create_host_form(forms.ModelForm):
     class Meta:
         model = Create_host_model
         fields = '__all__'
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,18 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from .forms import (
-                    compute_resource_form,
-                    create_host_form,
-                    profile_form
+                    Compute_resource_form,
+                    Create_host_form,
+                    Profile_form
                     )
-from django.http import HttpResponseRedirect,HttpResponse
-from .models import Compute_resource_model,Profile_model
+from django.http import (
+                         HttpResponseRedirect,
+                         HttpResponse
+                         )
+from .models import (
+                    Compute_resource_model,
+                    Profile_model
+                    )
 
 
 # Create your views here.
@@ -14,41 +20,47 @@ from .models import Compute_resource_model,Profile_model
 def home(request):
     return render(request, 'home.html')
 
+
 def compute_resource(request):
-    form = compute_resource_form()
-    return render(request,'compute_resource.html',{'title_name':'Create New Compute Resource', 'form':form})
+    form = Compute_resource_form()
+    return render(request, 'compute_resource.html', {'title_name': 'Create New Compute Resource', 'form': form})
+
 
 def post_data(request):
-    form = compute_resource_form(request.POST)
+    form = Compute_resource_form(request.POST)
     if form.is_valid():
         form.save(commit=True)
     return HttpResponseRedirect('/')
 
 
 def profile(request):
-    form = profile_form()
-    return render(request,'profile.html',{'title_name':'Profile', 'form':form})
+    form = Profile_form()
+    return render(request, 'profile.html', {'title_name': 'Profile', 'form': form})
+
 
 def post_profile(request):
-    form = profile_form(request.POST)
+    form = Profile_form(request.POST)
     if form.is_valid():
         form.save(commit=True)
     return HttpResponseRedirect('/')
 
+
 def create_host(request):
-    form = create_host_form()
-    return render(request,'create_host.html', {'title_name':'Create A New Host','form':form})
+    form = Create_host_form()
+    return render(request, 'create_host.html', {'title_name': 'Create A New Host', 'form': form})
+
 
 def post_create_host(request):
-    form = create_host_form(request.POST)
+    form = Create_host_form(request.POST)
     if form.is_valid():
         form_vm = form.cleaned_data['vm_name']
         form_os = form.cleaned_data['vm_os']
         form_profile = form.cleaned_data['select_vm_profile']
         profile_details = list(Profile_model.objects.filter(profile_name=form_profile).values_list()[0])
-        *not_imp1,ram,cpus,disk_size,compute_profile=profile_details
+        *not_imp1, ram, cpus, disk_size, compute_profile = profile_details
         compute_details = list(Compute_resource_model.objects.filter(name=compute_profile).values_list()[0])
-        *not_imp2,compute_ip,compute_passwd=compute_details
-        final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+form_vm+' --ram '+str(ram)+' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+form_vm+'.qcow2,bus=virtio,size='+str(disk_size)+' --network bridge:virbr0'
-    return render(request,'create_host.html',{'final_cmd':final_cmd})
+        *not_imp2, compute_ip, compute_passwd = compute_details
+        final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+form_vm+' --ram '+str(ram) \
+                   + '--vcpus'+str(cpus)+' --disk path=/var/lib/libvirt/images/'+form_vm+'.qcow2,bus=virtio,size='+str(disk_size)+' --network bridge:virbr0'
+    return render(request, 'create_host.html', {'final_cmd': final_cmd})
 
