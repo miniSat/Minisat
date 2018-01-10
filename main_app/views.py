4c48188534084c3401aca036a995b120435f8b99
Yadnesh Kulkarni <yadnesh45@gmail.com>
2018-01-10 18:11:37 +0530

    Move the dropdown of select compute from profile to create host for better usability
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

--- main_app/views.py
+++ main_app/views.py
@@ -81,15 +81,15 @@ def post_create_host(request):
         form_vm = form.cleaned_data['vm_name']
         form_os = form.cleaned_data['vm_os']
         form_profile = form.cleaned_data['select_vm_profile']
+        form_compute = form.cleaned_data['select_compute']
         profile_details = list(Profile_model.objects.filter(profile_name=form_profile).values_list()[0])
-        *not_imp1, ram, cpus, disk_size, compute_profile = profile_details
-        compute_details = list(Compute_resource_model.objects.filter(name=compute_profile).values_list()[0])
+        *not_imp1, ram, cpus, disk_size = profile_details
+        compute_details = list(Compute_resource_model.objects.filter(name=form_compute).values_list()[0])
         *not_imp2, compute_ip, compute_passwd = compute_details
         os_details = list(Operating_system_model.objects.filter(os_name=form_os).values_list()[0])
         *not_imp3, location_url = os_details
         final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+form_vm+' --ram '+str(ram) \
-                   + ' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+form_vm+'.qcow2,bus=virtio,size='+str(disk_size)+' --location '+location_url+' --network bridge:virbr0'
-
+                   + ' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+form_vm+'.qcow2,bus=virtio,size='+str(disk_size)+' --location '+location_url+' --network bridge:virbr0 &'
     os.system(final_cmd)
     return HttpResponseRedirect('/')
 
