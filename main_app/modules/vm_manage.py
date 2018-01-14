3593ada9cd4be7c59bd33f40e0ccc14d991f2668
Khomesh Thakre <khomeshthakre24@gmail.com>
2018-01-14 23:57:41 +0530

    Added modules (#40)
    * Ignore __pycache__
    
    Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>
    
    * Signed-off-by: khomesh24 <khomeshthakre24@gmail.com>
    
    Added modules
    - kickstart.py
    - vm_manage.py

---
+++ main_app/modules/vm_manage.py
@@ -0,0 +1,20 @@
+import os
+
+
+def vm_create(compute_ip, name, ram, cpus, disk_size, location_url):
+    final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+name+' --ram '+str(ram) \
+        + ' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+name+'.qcow2,bus=virtio,size='+str(disk_size)+' --location '+location_url+' --network bridge:virbr0 > /dev/null 2>&1 &'
+    print(final_cmd)
+    response = os.system(final_cmd)
+    if response == 0:
+        return True
+    else:
+        False
+
+def isOnline(host):
+    response = os.system("ping -c 2 "+ host +" > /dev/null 2>&1 &")
+    if response == 0:
+        print("online")
+    else:
+        print("offline")
+
