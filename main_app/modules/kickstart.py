d9aca133322013ab72a63c889a413ca18255a200
khomesh24 <khomeshthakre24@gmail.com>
2018-01-16 14:16:33 +0530

    resolve conflicts

--- main_app/modules/kickstart.py
+++ main_app/modules/kickstart.py
@@ -1,28 +1,24 @@
-def kick_gen(passwd, location, partition):
-    ks = open("ks.cfg", "w+")
-    ks.write("install \n"
-             "keyboard 'us' \n"
-             "rootpw --plaintext " + passwd + "\n"
-                                              "lang en_US \n"
-                                              "firewall --enabled \n"
-                                              "reboot \n"
-                                              "timezone Africa/Abidjan --isUtc \n"
-                                              "graphical \n"
-                                              "url --url= \"" + location + "\" \n"
-                                                                           "auth  --useshadow  --passalgo=sha512 \n"
-                                                                           "firstboot --enable \n"
-                                                                           "selinux --enforcing \n"
-                                                                           "bootloader --location=mbr \n"
-                                                                           "clearpart --all --initlabel\n"
-             )
-    for i in partition:
-        ks.write(i + "\n")
-    ks.close()
+import os
 
-
-passwd = "root"
-location = "http://172.22.26.203/repos/fedora25/"
-partition = ["part /boot --fstype=\"ext4\" --size=1024 ",
-             "part swap --fstype=\"swap\" --size=2048 ",
-             "part / --fstype=\"ext4\" --grow --size=1 "]
-kick_gen(passwd, location, partition)
+def kick_gen(passwd, location):
+    with open("/var/www/html/ks.cfg", "w+") as ks:
+        ks.write("install \n"
+                 "keyboard 'us' \n"
+                 "rootpw --plaintext "+passwd+"\n"
+                 "lang en_US \n"
+                 "firewall --enabled \n"
+                 "reboot \n"
+                 "timezone Africa/Abidjan --isUtc \n"
+                 "graphical \n"
+                 "url --url=\"" +location+ "\" \n"
+                 "auth  --useshadow  --passalgo=sha512 \n"
+                 "firstboot --disable \n"
+                 "selinux --enforcing \n"
+                 "bootloader --location=mbr \n"
+                 "clearpart --all --initlabel\n"
+                 "part /boot --fstype=\"ext4\" --size=1024 \n"
+                 "part swap --fstype=\"swap\" --size=2048 \n"
+                 "part / --fstype=\"ext4\" --grow --size=1"
+                 )
+    #os.system("scp /var/www/html/ks.cfg root@172.22.26.202:/var/www/html/")
+    return "http://172.22.26.202/ks.cfg"
