ce3ffc4b646536d7d40abdde09de14f7d4e5a598
jadhavganesh <jadhavganesh0301@gmail.com>
2018-01-17 22:29:54 +0530

    Resolve Conflict
    Signed-off-by: jadhavganesh <jadhavganesh0301@gmail.com>

--- main_app/modules/vm_manage.py
+++ main_app/modules/vm_manage.py
@@ -2,8 +2,10 @@ import os
 
 
 def vm_create(compute_ip, name, ram, cpus, disk_size, location_url, kickstart_location):
-    final_cmd = 'virt-install --connect qemu+ssh://root@'+compute_ip+'/system --name '+name+' --ram '+str(ram) \
-        + ' --vcpus '+str(cpus)+' --disk path=/var/lib/libvirt/images/'+name+'.qcow2,bus=virtio,size='+str(disk_size)+' --location '+location_url+' --extra-args=\'ks='+kickstart_location+' ksdevice=ens3\' --network bridge:virbr0 > /dev/null 2>&1 &'
+    final_cmd = 'virt-install --connect qemu+ssh://root@' + compute_ip + '/system --name ' + name + ' --ram ' + str(ram) \
+                + ' --vcpus ' + str(
+        cpus) + ' --disk path=/var/lib/libvirt/images/' + name + '.qcow2,bus=virtio,size=' + str(
+        disk_size) + ' --location ' + location_url + ' --extra-args=\'ks=' + kickstart_location + ' ksdevice=ens3\' --network bridge:virbr0 > /dev/null 2>&1 &'
     print(final_cmd)
     response = os.system(final_cmd)
     if response == 0:
