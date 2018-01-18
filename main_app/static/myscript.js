7d7e98bd153ca3511781425cc4d9d0d47c102545
CURIOSUER005 <shailesh.kolhapur@gmailcom>
2018-01-18 21:58:30 +0530

    Keep IP Validation on compute resource page only
    Signed-off-by: CURIOSUER005 <shailesh.kolhapur@gmailcom>

--- main_app/static/myscript.js
+++ main_app/static/myscript.js
@@ -1,22 +1,31 @@
 
 $(document).ready(function(e){
-     $("form").submit(function(e){
+     $("#compute_form").submit(function(e){
      var ip_addr=$('#ip_add').val();
      if (validateEmail(ip_addr)) {
-            }
+                      }
      else{
         alert('Invalid  IP Address');
         e.preventDefault();
          }
     });
+
 });
 
 function validateEmail(ip_addr) {
     var filter = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     if (filter.test(ip_addr)) {
-        return true;
+         return true;
     }
     else {
         return false;
     }
+}
+
+//Toast Msg function
+
+function myFunction() {
+    var x = document.getElementById("snackbar")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 }
