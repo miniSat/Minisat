5f6993006b34baa95864eb7c2c627388928d1349
Yadnesh Kulkarni <yadnesh45@gmail.com>
2018-02-02 23:40:36 +0530

    Make compute resource from use AJAX to submit data to user
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

--- main_app/static/myscript.js
+++ main_app/static/myscript.js
@@ -1,13 +1,64 @@
-
 $(document).ready(function(e){
-     $("#compute_form").submit(function(e){
-     var ip_addr=$('#ip_add').val();
-     if (validateEmail(ip_addr)) {
-         }
-     else{
+     $(document).on('submit','#compute_form',function(e){
+     var ip_addr=$('#id_ip_address').val();
+     result = validateEmail(ip_addr);
+     if (result == true) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: 'post_data',
+            data: {
+                'name': $("#id_name").val(),
+                'ip_address': $("#id_ip_address").val(),
+                'root_password': $("#id_root_password").val(),
+                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: function(data){
+                console.log(data['status']);
+                console.log(data['vm-status']);
+                console.log(data['docker-status']);
+                console.log(data);
+                if(Object.keys(data).length == 0)
+                {
+                    $("#results").html(
+                        '<div id="success-div" class="alert alert-success alert-dismissable fade in">\
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+                        <strong id="success-box">Successfully Added compute resource</strong>\
+                        </div>'
+                    );
+                }
+                else
+                {
+                    var result = "";
+                    for (var each in data){
+                        result = result + data[each] + "<br>"
+                    }
+                    console.log(result);
+                    $("#results").html(
+                        '<div id="error-div" class="alert alert-danger alert-dismissable fade in">\
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+                        <strong id="error-box"></strong>\
+                        </div>'
+                    );
+                    $("#error-box").html(result);
+                }
+            }
+        });
+    }
+    else if(result == 0){
+        $("#results").html(
+            '<div class="alert alert-danger alert-dismissable fade in">\
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>\
+            <strong>'+"Cannot add localhost as compute resource"+'</strong>\
+            </div>'
+        );
+        e.preventDefault();
+    }
+    else{
         alert('Invalid  IP Address');
         e.preventDefault();
-         }
+    }
     });
 
 });
@@ -15,7 +66,14 @@ $(document).ready(function(e){
 function validateEmail(ip_addr) {
     var filter = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
     if (filter.test(ip_addr)) {
-         return true;
+        if(ip_addr != "127.0.0.1")
+        {
+            return true;
+        }
+        else
+        {
+            return 0;
+        }
     }
     else {
         return false;
@@ -29,3 +87,8 @@ function myFunction() {
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 }
+
+
+$(document).on('submit','#compute_form',function(e){
+    
+});
