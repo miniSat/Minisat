53ee094cf58d6adb86f0e3945061039e482d6ebe
Yadnesh Kulkarni <yadnesh45@gmail.com>
2018-02-16 11:18:33 +0530

    Search for package in packages table
    Signed-off-by: Yadnesh Kulkarni <yadnesh45@gmail.com>

---
+++ satellite/static/vm_packages.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $.get('get_vm_packages',function(data){
+        console.log(data);
+    });
+});
+
+
+function myFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myPackages");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
