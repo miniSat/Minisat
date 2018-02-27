b4142799522cfbd91e7f8329e3fe0c0bca4ef7f7
CURIOUSER005 <shailesh.kolhapur@gmail.com>
2018-02-27 00:37:18 -0500

    remove unncessary libraries
    Signed-off-by: CURIOUSER005 <shailesh.kolhapur@gmail.com>

--- satellite/test_minisat.py
+++ satellite/test_minisat.py
@@ -1,9 +1,7 @@
 from django.test import TestCase  # NOQA
 import os
-import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 # Create your tests here.
 
 
@@ -31,28 +29,3 @@ def test_web_ui():
     driver.get("http://localhost:8000/local_images")
     assert "MiniSat" in driver.title
     driver.close()
-
-
-## will be used later
-
-# def test_compute_resource():
-#     options = Options()
-#     de = {}
-#     # options.add_argument('-headless')
-#     driver = webdriver.Firefox(firefox_options=options)
-#     driver.get("http://localhost:8000/compute_resource")
-#     name = driver.find_element_by_id("compute_name")
-#     name.send_keys("compute_testing")
-#     ip = driver.find_element_by_id("compute_ip")
-#     de["ip"] = str(os.popen("hostname -I").readlines()).split(" ")
-#     ip.send_keys(de["ip"][0][2:])
-#     root_pass = driver.find_element_by_id("compute_password")
-#     root_pass.send_keys("root")
-#     compute_submit = driver.find_element_by_id("compute_submit")
-#     compute_submit.send_keys(Keys.RETURN)
-#     time.sleep(11)
-#     driver.save_screenshot("compute.png")
-#     # assert "Compute Resource Added Successfully" in driver.page_source
-#     print(driver.page_source)
-#
-
