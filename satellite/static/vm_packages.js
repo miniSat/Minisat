4d09ddddc5744bf7d529979bcfb565f7bc1dd0ea
yadneshk <yadnesh45@gmail.com>
2018-03-01 23:21:57 +0530

    Use AJAX to fetch vm packages
    Signed-off-by: yadneshk <yadnesh45@gmail.com>

--- satellite/static/vm_packages.js
+++ satellite/static/vm_packages.js
@@ -1,3 +1,34 @@
+$(document).ready(function(){
+    var raw_vm_ip = $("#7").text();
+    var raw_compute_ip = $("#10").text();
+    var raw_vm_name = $("#2").text();
+    var compute_name=$("#10").text();
+    vm_ip = raw_vm_ip.split(":")[1].split("/")[0].trim();
+    vm_ip = vm_ip.replace(/\./g,'-');
+    compute_ip = raw_compute_ip.split("(")[1].split(")")[0].trim();
+    compute_ip = compute_ip.replace(/\./g,'-');
+    compute_name = compute_name.split(":")[1].split("(")[0].trim();
+    vm_name = raw_vm_name.split(":")[1].trim();
+    $("#vm-packages-spinner").show();
+    $.ajax({
+        url: '/get_vm_packages/'+compute_ip+'/'+compute_name+'/'+vm_ip+'/'+vm_name,
+        dataType: 'json',
+        success: function(result){
+            var package_table=document.getElementById("vm_packages_table");
+            var package_rows = "";
+            for (var each in result['packages'])
+            {
+                var ele = document.createElement("tr");
+                package_rows=package_rows+"<td>"+result['packages'][each]+"</td>";
+                ele.innerHTML=package_rows;
+                package_table.appendChild(ele);
+                package_rows = "";
+            }
+            $("#vm-packages-spinner").hide();
+        }
+    });
+});
+
 function myFunction() {
     var input, filter, table, tr, td, i;
     input = document.getElementById("myInput");
